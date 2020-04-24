7a6c1f23a120103b42a7ff9b3b68e62b2d058b85
Esmaeil Ahmadipour <software8686@gmail.com>
2020-04-25 03:10:34 +0430

    Commit> Configured Base App with Chopper Lib.

---
+++ lib/data/post_api_service.dart
@@ -0,0 +1,17 @@
+
+import 'package:chopper/chopper.dart';
+part 'post_api_service.chopper.dart';
+
+@ChopperApi(baseUrl: 'https://jsonplaceholder.typicode.com/posts')
+abstract class PostApiService extends ChopperService{
+
+  @Get()
+  Future<Response> getPosts();
+
+  @Get(path: '{id}')
+  Future<Response> getPost(@Path('id') int id);
+
+  @Post()
+  Future<Response> postPost(@Body() Map<String , dynamic> body,);
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutterchopper/app.dart';
+
+void main() => runApp(MyApp());
